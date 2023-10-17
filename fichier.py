d5bff3f1dd043fbfef082ab7bffe89ebf6f218af
Jocelyn-Bournier <bournierjocelyn@yahoo.fr>
2023-10-17 09:23:42 +0200

    first session

---
+++ fichier.py
@@ -0,0 +1,13 @@
+
+
+
+A = (None|T) x Color x User -> T
+
+(temps x temps) x A x matrices de valeur T -> matrice de valeur T
+
+lambda _, y : y 
+lambda x, _ : x+1
+def f(x,y):
+    x[y]+1
+    return x
+
