7076ee8ed21cfe1a43dda52532dbf2a2faffcae2
ajreckof <tbonhoure@ymail.Com>
2023-10-18 15:21:44 +0200

    save au cas ou

--- data.py
+++ data.py
@@ -2,45 +2,62 @@ import pandas as pd
 import glob
 import os
 
-colors = {
-    '#FFFFFF' : 0,
-    '#FF4500' : 1,
-    '#B44AC0' : 2,
-    '#3690EA' : 3,
-    '#00A368' : 4,
-    '#000000' : 5, 
-    '#FFD635' : 6,
-    '#FFA800' : 7,
-}
-
-def string_to_color(s : str):
-    return colors[s]
-
-def open_data(file):
-    df = pd.read_csv(os.path.join("cleaned",file))
-    # pour faire ca besoin de la 
-    df["timestamp"] = pd.to_datetime(df.timestamp, format = '%Y-%m-%d %H:%M:%S.%f %Z')
-    return df
-
-def clean_data(file : str):
-    file_path = os.path.split(file)
-    print(file_path)
-    if file_path[0] != "raw" :
-        raise ValueError(" file to be cleaned are only in the raw folder")
-    df = pd.read_csv(file)
-    df.timestamp.replace(":(..) UTC", ":$1.000 UTC", regex=True, inplace=True)
-    df.pixel_color.replace(colors, inplace=True)
-    df.to_csv("cleaned/" + file_path[1])
 
 
-def clean_all_files():
+startEndStamps = {}
+
+def check_data_chronologically_sorted(file):
+    df = open_data(file)
+    return df.timestamp.is_monotonic_increasing
+
+def check_all_files():
     folder_path = 'raw'
     file_extension = '*.csv'
 
     # Iterate over all .txt files in the folder
     for filename in glob.glob(os.path.join(folder_path, file_extension)):
-        print(filename)
+        check_data_chronologically_sorted(filename)
+
+
+def open_data(file):
+    print("start loading file:", file)
+    df = pd.read_csv(os.path.join("cleaned",file))
+    print("start formating data")
+    # pour faire ca besoin de la 
+    df.timestamp = pd.to_datetime(df.timestamp)
+    print("loaded file :", file)
+    return df
+
+def clean_data(file : str):
+    print(f"cleaning file {file}")
+    file_path = os.path.split(file)
+    if file_path[0] != "raw" :
+        raise ValueError(" file to be cleaned are only in the raw folder")
+    df = pd.read_csv(file)
+    df.timestamp.replace(":(..) UTC", r":\1.000 UTC", regex=True, inplace=True)
+    df.to_csv("cleaned/" + file_path[1])
+    startEndStamps[file_path[1]] = df.timestamp.iloc[0],df.timestamp.iloc[-1]
+
+
+def clean_all_files():
+    folder_path = 'raw'
+    file_extension = '*.csv'
+    
+    # Iterate over all .txt files in the folder
+    for filename in glob.glob(os.path.join(folder_path, file_extension)):
         clean_data(filename)
+    #save startStamps in a txt file
+    with open("startEndStamps.txt", "w") as f:
+        for key in startEndStamps.keys():
+            start,end = startEndStamps[key]
+            f.write(f"{key};{start};{end}\n")
 
-
-clean_all_files()
+def get_startStamps(reverse = False):
+    stamps = []
+    with open("startEndStamps.txt", "r") as f:
+        for line in f:
+            key, start, end = line.split(";")
+            end = end.removesuffix("\n")
+            stamps.append((pd.Timestamp(start), pd.Timestamp(end) , key))
+    return stamps
+    
---
+++ utils.py
@@ -0,0 +1,185 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+from data import open_data, get_startStamps
+from imageio import mimsave
+
+colors = {
+    '#FFFFFF' : [255,255,255],
+    '#FF4500' : [255,69,0],
+    '#B44AC0' : [180,74,192],
+    '#3690EA' : [54,144,234],
+    '#00A368' : [0,163,104],
+    '#000000' : [0,0,0], 
+    '#FFD635' : [255,214,53],
+    '#FFA800' : [255,168,0],
+}
+
+def string_to_color(s : str):
+    return colors[s]
+
+
+def string_to_coordinates(s : str):
+    l = s.split(",")
+    match len(l) :
+        case 2:
+            return [(int(l[1])+500, int(l[0])+500)]
+        case 3:
+            for i in range(3):
+                l[i] = l[i].removeprefix("{")
+                l[i] = l[i].removesuffix("}")
+                l[i] = l[i].strip()
+                l[i] = l[i].split(":")[1]
+
+            center = (int(l[1])+500, int(l[0])+500)
+            radius = int(l[2])
+
+            #generate all points from the circle
+            coords = []
+            for x in range(center[0] - radius, center[0] + radius):
+                for y in range(center[1] - radius, center[1] + radius):
+                    if (x - center[0])**2 + (y - center[1])**2 <= radius**2:
+                        coords.append((x,y))
+            
+            return coords
+        
+        case 4:
+            
+            left_up_corner = (int(l[1])+500, int(l[0])+500)
+            right_down_corner = (int(l[3])+500, int(l[2])+500)
+
+            #generate all points from the rectangle
+            coords = []
+            for x in range(left_up_corner[0], right_down_corner[0]):
+                for y in range(left_up_corner[1], right_down_corner[1]):
+                    coords.append((x,y))
+            
+            return coords
+
+
+def binary_search(arr, target):
+    """
+        get the first element in arr that is superior or equal to target
+    """
+    left, right = 0, len(arr) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid  # Element found, return its index
+        elif arr[mid] < target:
+            left = mid + 1  # Adjust the search range to the right half
+        else:
+            right = mid  # Adjust the search range to the left half
+    return left 
+
+def convertImage(image,transform):
+    #convert the image to RGB
+    rezImage = np.zeros((len(image),len(image[0]),3), dtype=np.uint8)
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            rezImage[i][j] = transform(image[i][j])
+    return rezImage
+
+
+def visualise(image, transform):
+    #visualise with plt a matrix of 1000x1000 with colors for each coordinate
+    image = convertImage(image,transform)
+    plt.imshow(image)
+    plt.show()
+
+def summary(startingTimeStamp, EndingTimeStamp, startingData, df, summary_function):
+    start_index = binary_search(df.timestamp, startingTimeStamp)
+    end_index = binary_search(df.timestamp, EndingTimeStamp)
+    df = df.iloc[start_index:end_index]
+    rezData = copy.deepcopy(startingData)
+    for row in df.itertuples() :
+        l = string_to_coordinates(row.coordinate)
+        is_mod = len(l) > 1
+        for coord in l :
+            x,y = coord
+            rezData[x][y] = summary_function(rezData[x][y],row.pixel_color, is_mod)
+    return rezData
+
+def visualise_at_interval(summary_function, transform, interval, startingState, rezfilename, startingTimeStamp = None, EndingTimeStamp=None, duration = None):
+
+    opened_data = {}
+    files_and_timestamp = get_startStamps()
+
+    if startingTimeStamp and startingTimeStamp > files_and_timestamp[0][0]:
+        EndingTimeStamps = [x[1] for x in files_and_timestamp]
+        idx = binary_search(EndingTimeStamps, startingTimeStamp)
+        currentTimeStamp = startingTimeStamp
+    else :
+        idx = 0
+        currentTimeStamp = files_and_timestamp[0][0]
+    if not EndingTimeStamp : 
+        EndingTimeStamp = files_and_timestamp[-1][1]
+    nextTimeStamp = currentTimeStamp + (duration if duration else interval)
+
+    _, nextFileTimeStamp, filename = files_and_timestamp[idx]
+    df = open_data(filename)
+    if duration :
+        opened_data[filename] = df
+
+    currentState = copy.deepcopy(startingState)
+    rez= [startingState]
+    start_idx = idx
+    while currentTimeStamp < EndingTimeStamp :
+        # make the summary
+        currentState = summary(currentTimeStamp, nextTimeStamp, currentState, df, summary_function)
+        if nextTimeStamp > nextFileTimeStamp and idx + 1 < len(files_and_timestamp) : 
+            idx+=1
+            _, nextFileTimeStamp, filename = files_and_timestamp[idx]
+            if filename in opened_data :
+                df = opened_data[filename]
+            else :
+                df = open_data(filename)
+                if duration :
+                    opened_data[filename] = df
+        else : 
+            if nextTimeStamp > nextFileTimeStamp and duration:
+                try :
+                    if (nextTimeStamp - nextFileTimeStamp) / duration > 0.5 :
+                        break
+                    currentState *= 1 - (nextTimeStamp - nextFileTimeStamp) / duration
+                except Exception as e:
+                    # Catch any exception without specifying a specific type.
+                    print(f"An error occurred: {e}")
+            # save the current state 
+            rez.append(currentState)
+
+            if duration : 
+                currentState = copy.deepcopy(startingState)
+                print(f"just made summary from {currentTimeStamp} to {nextTimeStamp}")
+            else :
+                print("just made summary until ", nextTimeStamp)
+            
+            # go to the next interval
+            currentTimeStamp += interval
+            nextTimeStamp +=interval
+            if duration:
+                _, nextFileTimeStamp, filename = files_and_timestamp[start_idx]
+                while nextFileTimeStamp < currentTimeStamp and idx + 1 < len(files_and_timestamp) :
+                    start_idx += 1
+                    del opened_data[filename]
+                    _, nextFileTimeStamp, filename = files_and_timestamp[start_idx]
+                
+                idx = start_idx
+                if filename in opened_data :
+                    df = opened_data[filename]
+                else :
+                    df = open_data(filename)
+                    opened_data[filename] = df
+                
+
+
+                
+    # make visualisation
+    transformed_rez = transform(rez)
+    
+    # save it in a gif 
+    mimsave(f"visualisation/{rezfilename}.gif", transformed_rez, duration= 3, loop= 0)
+    print(f"results saved on {rezfilename}.gif")
+
