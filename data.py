f6cab35fde8ee0007cda7f33365808fd89279cbb
ajreckof <tbonhoure@ymail.Com>
2023-11-18 12:55:33 +0100

    preétraitement et d'autre petit truc

--- data.py
+++ data.py
@@ -4,151 +4,100 @@ import os
 from pathlib import Path
 import gzip
 import zipfile
+import numpy as np
+
+from IPython.display import clear_output
+from inspect import cleandoc
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# # Specify the file(s) you want to download
-# files_to_download = ['file1.csv', 'file2.txt']  # Replace with the actual file names
 
-# # Specify the directory to save the downloaded files
-# download_path = './downloaded_data'
 
-# # Download each specified file individually
-# for file_name in files_to_download:
-#     api.dataset_download_file(dataset_slug, file_name, path=download_path, unzip=True)
+
+cached_log = {}
+internal_separator = "\n"
+intra_logger_separator = "\n\n"
+    
+
+class printer(str):
+    def __repr__(self):
+        return cleandoc(self)
+
+def update_output():
+    clear_output(True)
+    print(intra_logger_separator.join(cached_log.values()))
+
+def display(obj, display_id, internal_separator = "\n"):
+    global cached_log
+    if display_id in cached_log:
+        cached_log[display_id] += internal_separator + str(obj)
+    else : 
+        cached_log[display_id] = str(obj)
+    cached_log = {key:cached_log[key] for key in sorted(cached_log)}
+    update_output()
+
+def update_display(obj, display_id):
+    clear_display(display_id, wait = True)
+    display(obj,display_id)
+
+def clear_display(display_id, wait = False):
+    if display_id in cached_log :
+        del cached_log[display_id] 
+        if not wait :
+            update_output(wait)
+        
+
+
+class Logger:
+
+    def __init__(self, display_id : int | None = None, internal_separator = "\n") -> None:
+        if display_id is None:
+            display_id = max(cached_log.keys())
+        self.display_id = display_id
+        self.internal_separator = internal_separator
+    
+    def display(self, obj):
+        display(obj, self.display_id, self.internal_separator)
+    
+    def update(self, obj):
+        update_display(obj, self.display_id)
+    
+    def clear(self, wait = False):
+        clear_display(self.display_id, wait)
+
+
+
+
+
 
 def unzip_file(zip_file_path: str):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(os.path.dirname(zip_file_path))
     os.remove(zip_file_path)
-    
+
 
 def get_on_disk_file_name(kaggle_file_info):
     return str(kaggle_file_info)
 
+
 class StartStamps():
-    def  __init__(self, dataIterator) -> None:
+    def __init__(self, dataIterator) -> None:
         self.data = dataIterator
         self._startStamps = dataIterator.init_startStamps()
-    
+
     def __len__(self):
         return len(self.data)
-    
-    def __getitem__(self, index : int):
+
+    def __getitem__(self, index: int):
         index = index if index >= 0 else len(self) + index
         self.data.make_usable(index)
         return self._startStamps[index]
-        
+
     def __setitem__(self, index, value):
         self._startStamps[index] = value
 
     def __delitem__(self, key):
         raise TypeError("Cannot delete items in DataIteratorDownload")
-    
-
-
-class DataIteratorDownload():
-    def __init__(self, dataset, raw_data_folder = "raw/",cleaned_data_folder = "cleaned/", startEndStampsFile= "startEndStamps.txt"):
-        # Instantiate the Kaggle API
-        self.api = KaggleApi()
-        self.raw_data_folder = raw_data_folder
-        self.cleaned_data_folder = cleaned_data_folder
-        self.startEndStampsFile = startEndStampsFile
-        # Authenticate with your Kaggle API credentials
-        self.api.authenticate()
-        self.dataset = dataset
-        self.files = sorted([get_on_disk_file_name(file) for file in self.api.dataset_list_files(self.dataset).files])[:3]
-        self.startStamps = StartStamps(self)
-
-
-
-    def __iter__(self):
-        return ListLikeIterator(self)
-
-    def make_usable(self, index : int):
-        if index >= len(self.files):
-            raise IndexError("Index out of range.")
-        file = self.files[index]
-
-        if not Path(self.raw_data_folder + file).exists():
-            self.api.dataset_download_file(self.dataset, file, path=self.raw_data_folder)
-            unzip_file(self.raw_data_folder + file + ".zip")
-
-        if not Path(self.cleaned_data_folder + file).exists():
-            self.clean_data(index)
-    
-    def __getitem__(self, index : int):
-        index = index if index >= 0 else len(self) + index
-        self.make_usable(index)
-        return self.open_data(index)
-        
-    def open_data(self, index: int):
-        file = self.files[index]
-        print("start loading file:", file)
-        df = pd.read_csv(gzip.open(os.path.join(self.cleaned_data_folder,file)))
-        print("start formating data")
-        # pour faire ca besoin de la 
-        df.timestamp = pd.to_datetime(df.timestamp)
-        print("loaded file :", file)
-        return df
-
-    def __setitem__(self, index, value):
-        raise TypeError("Cannot set items in DataIteratorDownload")
-
-    def __delitem__(self, key):
-        raise TypeError("Cannot delete items in DataIteratorDownload")
-
-    def __len__(self):
-        return len(self.files)
-
-    def check_data_chronologically_sorted(self,file):
-        df = self.open_data(file)
-        return df.timestamp.is_monotonic_increasing
-
-    def clean_data(self,idx : int):
-        file = self.files[idx]
-        print(f"cleaning file {file}")
-        if not Path(self.raw_data_folder + file):
-            raise ValueError(f"The file {file} is not present in the raw folder")
-        df = pd.read_csv(gzip.open(self.raw_data_folder + file))
-        df.timestamp.replace(":(..) UTC", r":\1.000 UTC", regex=True, inplace=True)
-        df.to_csv(self.cleaned_data_folder + file, index=False, compression='gzip')
-        start,end = df.timestamp.iloc[0],df.timestamp.iloc[-1]
-        self.startStamps[idx] = (pd.Timestamp(start),pd.Timestamp(end))
-        #save startStamps in a txt file
-        with open(self.startEndStampsFile, "a") as f:
-            f.write(f"{idx};{start};{end}\n")
-    
-    def check_all_files_downloaded(self):
-        folder_path = 'raw'
-        file_extension = '*.csv'
-
-        # Iterate over all .txt files in the folder
-        for filename in glob.glob(os.path.join(folder_path, file_extension)):
-            self.check_data_chronologically_sorted(filename)
-    
-    def clear_cleaned_files(self):
-        # TODO
-        pass
-
-    def clean_all_files(self):
-        folder_path = 'raw'
-        file_extension = '*.csv'
-        
-        # Iterate over all .txt files in the folder
-        for filename in glob.glob(os.path.join(folder_path, file_extension)):
-            self.clean_data(filename)
-    
-    def init_startStamps(self,reverse = False):
-        stamps = {}
-        with open(self.startEndStampsFile, "r") as f:
-            for line in f:
-                index, start, end = line.split(";")
-                end = end.removesuffix("\n")
-                stamps[int(index)] = (pd.Timestamp(start), pd.Timestamp(end))
-        return stamps
-
-
 
 
 class ListLikeIterator:
@@ -166,17 +115,174 @@ class ListLikeIterator:
             return result
         else:
             raise StopIteration
-    
+
+
+class DataIteratorDownload():
+    def __init__(self, dataset, raw_data_folder="raw/", cleaned_data_folder="cleaned/", startEndStampsFile="startEndStamps.txt", datasetStartTimeStampFile="datasetStartTimeStamp.txt", manually_downloaded=False):
+        # Instantiate the Kaggle API
+        self.api = KaggleApi()
+        self.raw_data_folder = raw_data_folder
+        self.cleaned_data_folder = cleaned_data_folder
+        self.startEndStampsFile = startEndStampsFile
+        # Authenticate with your Kaggle API credentials
+        self.api.authenticate()
+        self.dataset = dataset
+        self.files = sorted(
+            [str(file) for file in self.api.dataset_list_files(self.dataset).files])
+        if not manually_downloaded:
+            self.files = self.files[:20]
+        self.startStamps = StartStamps(self)
+        self.cache = {}
+        self.datasetStartTimeStampFile = datasetStartTimeStampFile
+        self.dh = Logger(display_id=0)
+
+        try:
+            with open(self.datasetStartTimeStampFile, "r") as f:
+                self.datasetStartTimeStamp = int(f.read())
+        except FileNotFoundError:
+            # If the file doesn't exist this means there was no data stored
+            self.datasetStartTimeStamp = None
+
+        # ensure raw and cleaned data folder are present as they are not in the git to avoid bloating
+        try:
+            os.makedirs(self.raw_data_folder)
+            print(f"Folder '{self.raw_data_folder}' created.")
+        except FileExistsError:
+            pass
+
+        try:
+            os.makedirs(self.cleaned_data_folder)
+            print(f"Folder '{self.cleaned_data_folder}' created.")
+        except FileExistsError:
+            pass
+
+    def __iter__(self):
+        return ListLikeIterator(self)
+
+    def __setitem__(self, index, value):
+        raise TypeError("Cannot set items in DataIteratorDownload")
+
+    def __delitem__(self, index):
+        del self.cache[index]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index: int):
+        index = index if index >= 0 else len(self) + index
+        self.make_usable(index)
+        return self.open_data(index)
+
+    def make_usable(self, index: int):
+        if index >= len(self.files):
+            raise IndexError("Index out of range.")
+        file = self.files[index]
+
+        if not Path(self.raw_data_folder + file).exists():
+            self.api.dataset_download_file(
+                self.dataset, file, path=self.raw_data_folder)
+            unzip_file(self.raw_data_folder + file + ".zip")
+
+        if not Path(self.cleaned_data_folder + file).exists():
+            self.clean_data(index)
+
+    def open_data(self, index: int):
+        if index in self.cache:
+            return self.cache[index]
+
+        file = self.files[index]
+        self.dh.update(printer(f"start loading file : {file}"))
+        df = pd.read_csv(gzip.open(os.path.join(
+            self.cleaned_data_folder, file)))  # type: ignore
+        self.dh.update(printer(f"loaded file : {file}"))
+        self.cache[index] = df
+        return df
+
+    def clean_data(self, idx: int):
+        file = self.files[idx]
+        self.dh.update(printer(f"cleaning file : {file}"))
+
+        if not Path(self.raw_data_folder + file):
+            raise ValueError(
+                f"The file {file} is not present in the raw folder")
+
+        # type: ignore
+        df = pd.read_csv(gzip.open(self.raw_data_folder + file))
+
+        df.timestamp.replace(":(..) UTC", r":\1.000 UTC",
+                             regex=True, inplace=True)
+        df.timestamp = pd.to_datetime(df.timestamp).astype(int) // 10**6
+        if idx == 0:
+            self.datasetStartTimeStamp = df.timestamp.iloc[0]
+            with open(self.datasetStartTimeStampFile, "w") as f:
+                f.write(str(self.datasetStartTimeStamp))
+        df.timestamp = df.timestamp - self.get_datasetStartTimeStamp()
+
+        start, end = df.timestamp.iloc[0], df.timestamp.iloc[-1]
+        self.startStamps[idx] = (start, end)
+        # save startStamps in a txt file
+        with open(self.startEndStampsFile, "a") as f:
+            f.write(f"{idx};{start};{end}\n")
+        
+        df.to_csv(self.cleaned_data_folder + file,
+                  index=False, compression='gzip')
+
+        self.dh.update(printer(f"file cleaned : {file}"))
 
 
 
+    def get_datasetStartTimeStamp(self) -> int:
+        if self.datasetStartTimeStamp is None:
+            self.make_usable(0)
+        if self.datasetStartTimeStamp is None:
+            raise RuntimeError(
+                "make_usable forgot to set datasetStartTimeStamp when given index 0")
 
+        return self.datasetStartTimeStamp
 
+    def clear_cleaned_files(self):
+        # List all files in the folder
+        files = os.listdir(self.cleaned_data_folder)
 
+        # Iterate through the files and delete them
+        for file in files:
+            file_path = os.path.join(self.cleaned_data_folder, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
+        if os.path.isfile(self.startEndStampsFile):
+            os.remove(self.startEndStampsFile)
 
+        if os.path.isfile(self.datasetStartTimeStampFile):
+            os.remove(self.datasetStartTimeStampFile)
 
+        print(f"All files of cleaned data have been deleted.")
 
+        pass
 
+    def init_startStamps(self, reverse=False):
+        stamps = {}
 
-    
+        try:
+            # Try to open the file for reading
+            with open(self.startEndStampsFile, "r") as f:
+                for line in f:
+                    index, start, end = line.split(";")
+                    end = end.removesuffix("\n")
+                    stamps[int(index)] = (int(start), int(end))
+        except FileNotFoundError:
+            # If the file doesn't exist this means there was no data stored
+            pass
+        return stamps
+
+    def check_all_files_downloaded(self):
+        folder_path = 'raw'
+        file_extension = '*.csv'
+
+        # Iterate over all .txt files in the folder
+        for filename in glob.glob(os.path.join(folder_path, file_extension)):
+            self.check_data_chronologically_sorted(filename)
+
+    def check_data_chronologically_sorted(self, file):
+        df = self.open_data(file)
+        return df.timestamp.is_monotonic_increasing
--- utils.py
+++ utils.py
@@ -1,40 +1,44 @@
-
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
 from imageio import mimsave
-from IPython.display import display
-from inspect import cleandoc
 
-from data import DataIteratorDownload
-data = DataIteratorDownload("ricobruland/reddit-2023-rplace-pixel-data")
-
-colors = {
-    '#FFFFFF' : [255,255,255],
-    '#FF4500' : [255,69,0],
-    '#B44AC0' : [180,74,192],
-    '#3690EA' : [54,144,234],
-    '#00A368' : [0,163,104],
-    '#000000' : [0,0,0], 
-    '#FFD635' : [255,214,53],
-    '#FFA800' : [255,168,0],
-}
-
-class printer(str):
-	def __repr__(self):
-		return cleandoc(self)
+from data import DataIteratorDownload, Logger
+data = None
 
 
+def initialize_data_loader(**kwargs):
+    global data
+    data = DataIteratorDownload(
+        "ricobruland/reddit-2023-rplace-pixel-data", **kwargs)
+    return data
 
-def string_to_color(s : str):
-    return colors[s]
+
+colors = {}
 
 
-def string_to_coordinates(s : str):
+def string_to_color(hex_color: str):
+    global colors
+    if hex_color in colors:
+        return colors[hex_color]
+    else:
+        hex_color_no_hashtag = hex_color.removeprefix('#')
+
+        # Convert hex to RGB
+        rgb = [int(hex_color_no_hashtag[i:i+2], 16) for i in range(0, 6, 2)]
+        colors[hex_color] = rgb
+        return rgb
+
+
+def string_to_coordinates(s: str):
+    def translate(coord):
+        return (int(coord[1])+500, int(coord[0])+1000)
+
     l = s.split(",")
-    match len(l) :
+    match len(l):
         case 2:
-            return [(int(l[1])+500, int(l[0])+500)]
+            return [translate(l)]
         case 3:
             for i in range(3):
                 l[i] = l[i].removeprefix("{")
@@ -42,30 +46,32 @@ def string_to_coordinates(s : str):
                 l[i] = l[i].strip()
                 l[i] = l[i].split(":")[1]
 
-            center = (int(l[1])+500, int(l[0])+500)
+            center = translate(l[:2])
             radius = int(l[2])
 
-            #generate all points from the circle
+            # generate all points from the circle
             coords = []
             for x in range(center[0] - radius, center[0] + radius):
                 for y in range(center[1] - radius, center[1] + radius):
                     if (x - center[0])**2 + (y - center[1])**2 <= radius**2:
-                        coords.append((x,y))
-            
-            return coords
-        
-        case 4:
-            
-            left_up_corner = (int(l[1])+500, int(l[0])+500)
-            right_down_corner = (int(l[3])+500, int(l[2])+500)
+                        coords.append((x, y))
 
-            #generate all points from the rectangle
+            return coords
+
+        case 4:
+
+            left_up_corner = center = translate(l[:2])
+            right_down_corner = center = translate(l[2:])
+
+            # generate all points from the rectangle
             coords = []
             for x in range(left_up_corner[0], right_down_corner[0]):
                 for y in range(left_up_corner[1], right_down_corner[1]):
-                    coords.append((x,y))
-            
+                    coords.append((x, y))
+
             return coords
+        case _:
+            raise ValueError("String did not have proper format")
 
 
 def binary_search(arr, target):
@@ -82,11 +88,12 @@ def binary_search(arr, target):
             left = mid + 1  # Adjust the search range to the right half
         else:
             right = mid  # Adjust the search range to the left half
-    return left 
+    return left
 
-def convertImage(image,transform):
-    #convert the image to RGB
-    rezImage = np.zeros((len(image),len(image[0]),3), dtype=np.uint8)
+
+def convertImage(image, transform):
+    # convert the image to RGB
+    rezImage = np.zeros((len(image), len(image[0]), 3), dtype=np.uint8)
     for i in range(len(image)):
         for j in range(len(image[i])):
             rezImage[i][j] = transform(image[i][j])
@@ -94,120 +101,166 @@ def convertImage(image,transform):
 
 
 def visualise(image, transform):
-    #visualise with plt a matrix of 1000x1000 with colors for each coordinate
-    image = convertImage(image,transform)
+    # visualise with plt a matrix of 1000x1000 with colors for each coordinate
+    image = convertImage(image, transform)
     plt.imshow(image)
     plt.show()
+
 
 def summary(startingTimeStamp, EndingTimeStamp, startingData, df, summary_function):
     start_index = binary_search(df.timestamp, startingTimeStamp)
     end_index = binary_search(df.timestamp, EndingTimeStamp)
     df = df.iloc[start_index:end_index]
     rezData = copy.deepcopy(startingData)
-    for row in df.itertuples() :
+    for row in df.itertuples():
         l = string_to_coordinates(row.coordinate)
         is_mod = len(l) > 1
-        for coord in l :
-            x,y = coord
-            rezData[x][y] = summary_function(rezData[x][y],row.pixel_color, row.user, is_mod)
+        for coord in l:
+            x, y = coord
+            try:
+                rezData[x][y] = summary_function(
+                    rezData[x][y], row.pixel_color, row.user, is_mod)
+            except Exception as e:
+                print(x, y)
+                print(row.coordinate)
+                raise e
     return rezData
 
-def visualise_at_interval(summary_function, transforms, interval, startingState, rezfilename, startingTimeStamp = None, EndingTimeStamp=None, duration = None):
+
+TimePassed = pd.Timedelta | int
+
+
+class DynamicList:
+    def __init__(self, original_list, func):
+        self.original_list = original_list
+        self.func = func
+
+    def __len__(self):
+        return len(self.original_list)
+
+    def __getitem__(self, index):
+        return self.func(self.original_list[index])
+
+    def __setitem__(self, index, value):
+        raise PermissionError("This is a view as such you can't modify it")
+
+    def __delitem__(self, key):
+        raise PermissionError("This is a view as such you can't modify it")
+
+
+def convert_to_milliseconds(value: TimePassed | None):
+    if isinstance(value, pd.Timedelta):
+        return value.total_seconds() * 1000
+    else:
+        return value
+
+
+def visualise_at_interval(summary_function,
+                          transforms,
+                          interval: TimePassed,
+                          startingState,
+                          rezfilename,
+                          startingTimeStamp: TimePassed = 0,
+                          EndingTimeStamp: TimePassed = -1,
+                          duration: TimePassed = -1):
+    # make sure data exists
+    global data
+    if not data:
+        data = initialize_data_loader()
 
     i = 0
-    dh = display(printer("\r"), display_id=True)
-    opened_data = {}
+
+    # Create a display on which most thing from this function will be printed
+    dh = Logger(display_id=1)
+    if not dh:
+        raise RuntimeError(
+            "I don't how it happened but it didn't create a display")
+
+    # Get startStamps from the data
     files_and_timestamp = data.startStamps
 
-    if startingTimeStamp and startingTimeStamp > files_and_timestamp[0][0]:
-        EndingTimeStamps = [x[1] for x in files_and_timestamp]
-        idx = binary_search(EndingTimeStamps, startingTimeStamp)
-        currentTimeStamp = startingTimeStamp
-    else :
-        idx = 0
-        currentTimeStamp = files_and_timestamp[0][0]
-    if not EndingTimeStamp : 
+    # Transform Timedelta to ms
+    startingTimeStamp = convert_to_milliseconds(startingTimeStamp)
+    EndingTimeStamp = convert_to_milliseconds(EndingTimeStamp)
+    duration = convert_to_milliseconds(duration)
+    interval = convert_to_milliseconds(interval)
+
+    # Give actual default value to EndingTimeStamp
+    if EndingTimeStamp < 0:
         EndingTimeStamp = files_and_timestamp[-1][1]
-    nextTimeStamp = currentTimeStamp + (duration if duration else interval)
+
+    idx = binary_search(DynamicList(files_and_timestamp,
+                        lambda x: x[1]), startingTimeStamp)
+    currentTimeStamp = startingTimeStamp
+    nextTimeStamp = currentTimeStamp + (duration if duration > 0 else interval)
 
     _, nextFileTimeStamp = files_and_timestamp[idx]
     df = data[idx]
-    if duration :
-        opened_data[idx] = df
 
     currentState = copy.deepcopy(startingState)
-    rez= [startingState]
+    rez = [startingState]
     start_idx = idx
 
-    while currentTimeStamp < EndingTimeStamp :
+    while currentTimeStamp < EndingTimeStamp:
         # make the summary
-        currentState = summary(currentTimeStamp, nextTimeStamp, currentState, df, summary_function)
-        if nextTimeStamp > nextFileTimeStamp and idx + 1 < len(files_and_timestamp) : 
-            idx+=1
+        currentState = summary(
+            currentTimeStamp, nextTimeStamp, currentState, df, summary_function)
+        if nextTimeStamp > nextFileTimeStamp and idx + 1 < len(files_and_timestamp):
+            idx += 1
             _, nextFileTimeStamp = files_and_timestamp[idx]
-            if idx in opened_data :
-                df = opened_data[idx]
-            else :
-                df = data[idx]
-                if duration :
-                    opened_data[idx] = df
-        else : 
-            if nextTimeStamp > nextFileTimeStamp and duration:
-                try :
-                    if (nextTimeStamp - nextFileTimeStamp) / duration > 0.5 :
+            df = data[idx]
+        else:
+            if nextTimeStamp > nextFileTimeStamp and duration > 0:
+                try:
+                    if (nextTimeStamp - nextFileTimeStamp) / duration > 0.5:
                         break
-                    currentState *= 1 - (nextTimeStamp - nextFileTimeStamp) / duration
+                    currentState *= 1 - \
+                        (nextTimeStamp - nextFileTimeStamp) / duration
                 except Exception as e:
                     # Catch any exception without specifying a specific type.
                     print(f"An error occurred: {e}")
-            # save the current state 
+            # save the current state
             rez.append(currentState)
 
-            i+= 1
-            if duration : 
+            i += 1
+            if duration > 0:
                 currentState = copy.deepcopy(startingState)
-                dh.update(printer(
+                dh.update(
                     f"""
-                        just made summary from {currentTimeStamp} to {nextTimeStamp}
+                        just made summary from {pd.Timedelta(milliseconds=currentTimeStamp)} to {pd.Timedelta(milliseconds=nextTimeStamp)}
                         epochs : i
                     """
-                ))
-            else :
-                dh.update(printer(f"just made summary until {nextTimeStamp}"))
-
-
+                )
+            else:
+                dh.update(f"just made summary until {pd.Timedelta(milliseconds=nextTimeStamp)}")
 
             # go to the next interval
             currentTimeStamp += interval
-            nextTimeStamp +=interval
-            if duration:
+            nextTimeStamp += interval
+
+            _, nextFileTimeStamp = files_and_timestamp[start_idx]
+            while nextFileTimeStamp < currentTimeStamp and idx + 1 < len(data):
+                del data[start_idx]
+                start_idx += 1
                 _, nextFileTimeStamp = files_and_timestamp[start_idx]
-                while nextFileTimeStamp < currentTimeStamp and idx + 1 < len(data) :
-                    start_idx += 1
-                    del opened_data[start_idx]
-                    _, nextFileTimeStamp = files_and_timestamp[start_idx]
-                
-                idx = start_idx
-                if idx in opened_data :
-                    df = opened_data[idx]
-                else :
-                    df = data[idx]
-                    opened_data[idx] = df
-                
 
+            idx = start_idx
+            df = data[idx]
 
-    try :
-        for name, transform in transforms.items() :
+    dh.update("start making visualizations")
+    try:
+        for name, transform in transforms.items():
             # make visualisation
             transformed_rez = transform(rez)
-            # save it in a gif 
-            mimsave(f"visualisation/{rezfilename(name)}.gif", transformed_rez, duration= 3, loop= 0)
+            # save it in a gif
+            mimsave(f"visualisation/{rezfilename(name)}.gif",
+                    transformed_rez, duration=3, loop=0)
             print(f"results saved on {rezfilename(name)}.gif")
     except Exception as e:
         print(type(e))
         # make visualisation
         transformed_rez = transforms(rez)
-        # save it in a gif 
-        mimsave(f"visualisation/{rezfilename}.gif", transformed_rez, duration= 3, loop= 0)
+        # save it in a gif
+        mimsave(f"visualisation/{rezfilename}.gif",
+                transformed_rez, duration=3, loop=0)
         print(f"results saved on {rezfilename}.gif")
-
